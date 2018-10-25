dea08cb58f19d2545c1765013a0bf2be19fa6c12
Coty Richardson <crichardson90@live.com>
2018-10-24 20:46:43 -0500

    Added_Validation

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -20,21 +20,25 @@ class BlogPostsController < ApplicationController
    def show
     id = params[:id]
     @blog_post = BlogPost.find(id)
+    @new_comment = Comment.new
    end
 
    def new
+     @blog_post = BlogPost.new
      @tags = Tag.all
    end
 
    def create
-     blog_post = BlogPost.create(title: params[:title], content: params[:content])
+     @blog_post = BlogPost.new(title: params[:title], content: params[:content])
 
-     params[:tag_ids].each do |tag_id|
-      BlogPostTag.create(blog_post_id: blog_post.id,tag_id: tag_id)
-                                            
-   end
+     if @blog_post.save
+      @blog_post.create_tags(params[:tag_ids]) if params[:tag_ids]
 
-     redirect_to "/blog_posts/#{blog_post.id}"
+      redirect_to ("/blog_posts")
+    else
+      @tags = Tag.all
+      render 'new'
+     end
    end
 
   def edit
@@ -44,22 +48,16 @@ class BlogPostsController < ApplicationController
   end
 
   def update
-    blog_post = BlogPost.find(params[:id])
-    blog_post.update(title: params[:title], content: params[:content])
-
-    submitted_tag_ids = params[:tag_ids].map { |tag_id| tag_id.to_i }
-    old_tag_ids = blog_post.tag_ids - submitted_tag_ids
-    new_tag_ids = submitted_tag_ids - blog_post.tag_ids
-
-    old_tag_ids.each do |tag_id|
-      BlogPostTag.find_by(blog_post_id: blog_post.id, tag_id: tag_id).destroy
+    @blog_post = BlogPost.find(params[:id])
+    
+    if @blog_post.update(title: params[:title], content: params[:content])
+       @blog_post.update_tags(params[:tag_ids]) if params[:tag_ids]
+      
+      redirect_to "/blog_posts/#{@blog_post.id}"
+    else
+      @tags = Tag.all
+      render 'edit'
     end
-
-    new_tag_ids.each do |tag_id|
-      BlogPostTag.create(blog_post_id: blog_post.id, tag_id: tag_id)
-    end
-
-    redirect_to "/blog_posts/#{blog_post.id}"
   end
 
   def destroy
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,17 @@
 class CommentsController < ApplicationController
   def create
-    comment = Comment.create(
+    @new_comment = Comment.new(
                               username: params[:username],
                               content: params[:content],
                               blog_post_id: params[:blog_post_id]
                              )
-    redirect_to "/blog_posts/#{comment.blog_post_id}"
+  if @new_comment.save
+    redirect_to "/blog_posts/#{@new_comment.blog_post_id}"
+  else
+    @blog_post = BlogPost.find(params[:blog_post_id])
+    render '/blog_posts/show'
   end
+end
 
   def destroy
     blog_post_id = Comment.find(params[:id]).blog_post_id
